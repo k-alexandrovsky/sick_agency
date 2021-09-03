52c8bf07f47f2dc702e31187d1450efc2ba3e531
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2021-09-03 10:04:54 +0300

    start the server

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+
 app.post('/submit', (req, res)=>{
     console.log(req.body);
     res.sendStatus(200);
 });
+
+app.listen(4444);
