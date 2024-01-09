7f356d725493470d4805277b794c182584f5d802
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2024-01-09 17:04:09 +0100

    configurable MG domain

--- api/submit.js
+++ api/submit.js
@@ -8,12 +8,13 @@ const mailgun = new Mailgun(formData).client({
 });
 
 const MAIL_INBOX = process.env.MAIL_INBOX||'sickpick@sick.agency';
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN||'sick.agency';
 
 module.exports = async(req, res)=>{
     if (req.method!='POST')
         return res.status(405).send('Method Not Allowed');
-    await mailgun.messages.create('sick.agency', {
-        from: 'noreply@sick.agency',
+    await mailgun.messages.create(MAILGUN_DOMAIN, {
+        from: 'sick.agency@'+MAILGUN_DOMAIN,
         to: [MAIL_INBOX],
         subject: 'New request from '+req.body.email,
         text: Object.entries(req.body).map(([k, v])=>`${k}: ${v}`).join('\n'),
