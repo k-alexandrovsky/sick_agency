467ff9315652dec27d40671d9bf9b499c631bfba
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2024-01-09 16:54:09 +0100

    send from sick.agency

--- api/submit.js
+++ api/submit.js
@@ -12,8 +12,8 @@ const MAIL_INBOX = process.env.MAIL_INBOX||'sickpick@sick.agency';
 module.exports = async(req, res)=>{
     if (req.method!='POST')
         return res.status(405).send('Method Not Allowed');
-    await mailgun.messages.create('noreply.sick.agency', {
-        from: 'noreply@noreply.sick.agency',
+    await mailgun.messages.create('sick.agency', {
+        from: 'noreply@sick.agency',
         to: [MAIL_INBOX],
         subject: 'New request from '+req.body.email,
         text: Object.entries(req.body).map(([k, v])=>`${k}: ${v}`).join('\n'),
